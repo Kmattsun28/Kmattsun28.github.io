d570e82b0ea1793a36e866914713b4ca58680604
Kmattsun28 <142275205+Kmattsun28@users.noreply.github.com>
2026-04-10 05:51:19 +0000

    Deploying to gh-pages from @ Kmattsun28/Kmattsun28.github.io@8129e863ee677c1c67b263db9f8ab96ab87267c8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Kmattsun28/github.io",
+    "data-repo": "Kmattsun28/Kmattsun28.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
